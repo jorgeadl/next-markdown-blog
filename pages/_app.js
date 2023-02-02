9768f23df62ae2e6e28924ef154f57001e61df88
Jorge Suazo <jorge.s@desafiolatam.com>
2023-02-02 18:21:56 -0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import Header from '@/components/Header'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+    <>
+      <Header />
+      <main className='container'>
+        <Component {...pageProps} />
+      </main>
+      
+    </>
+  )
 }
